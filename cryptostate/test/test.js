16fa848363215901498a80eda270a7eed109b6dc
AkhileshGoswami <agiri5375@gmail.com>
2021-10-29 23:21:42 +0530

    testing done of mintCrypto function work's fine

--- cryptostate/test/test.js
+++ cryptostate/test/test.js
@@ -23,4 +23,16 @@ contract(CryptoState, ([deployer, author, tipper]) => {
       assert.equal(name, "Govt of India Cryptostate Poratal")
     })
   })
+  it("putting state on Cryptostate", async () => {
+    let listingPrice = await cryptostate.getListingPrice()
+    listingPrice = listingPrice.toString();
+    const created = await cryptostate.mintCryptoState("goswami  Niwas",
+    "agiri5375@gmai.com",
+    100,
+    "city Bhopal Mp 456333",
+    "0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0",
+    1,{value:listingPrice}
+    );
+console.log(created)
+  });
   })
