a3119144a8cbea2ab6c36899bcb195f486dd06eb
Prateek <singhprateek2704@gmail.com>
2021-10-30 11:48:36 +0530

    Made navbar responsive

--- cryptostate/src/App.jsx
+++ cryptostate/src/App.jsx
@@ -3,9 +3,9 @@ import React from "react";
 import Navbar from './Componenets/navbar/Navbar'
 function App() {
   return (
-    <div className="App">
-    <Navbar/>
-    </div>
+    <>
+      <Navbar/>
+    </>
 
   );
 }
--- cryptostate/src/index.js
+++ cryptostate/src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+<<<<<<< Updated upstream
+=======
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+
+
+>>>>>>> Stashed changes
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
