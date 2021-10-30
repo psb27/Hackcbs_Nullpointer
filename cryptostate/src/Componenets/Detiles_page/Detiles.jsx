6714aa374d28dc1230ee5926b32d2b2d26e535c5
AkhileshGoswami <agiri5375@gmail.com>
2021-10-30 12:56:10 +0530

    card compoenet careted

--- cryptostate/src/Componenets/Detiles_page/Detiles.jsx
+++ cryptostate/src/Componenets/Detiles_page/Detiles.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useLocation } from 'react-router'
 
 function Detiles() {
+
+    const loaction = useLocation()
+
+    console.log(loaction.state.owner)
+
     return (
         <div>
-            
+            <img src={loaction.state.Image}alt="" />
+            this is router
         </div>
     )
 }
--- cryptostate/src/Componenets/Register_page/Register.jsx
+++ cryptostate/src/Componenets/Register_page/Register.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Register() {
     return (
-        <div>
-            
+    <div classsName="main">
+
         </div>
     )
 }
